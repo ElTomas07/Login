29d36e07859ce320f3b7e8f28cbdd3223e44eba9
unknown <SENA@ARMAPRCCIFOD588.FormacionSena.RED>
2024-02-26 08:23:42 -0500

    Se realiza Task Scrud

--- src/routes/task.routes.js
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express'
-import { authRequired } from '../middlewares/validateToken.js'
-
-const router = Router()
-
-router.get('/task', authRequired, (req, res) => res.send('task'))
-
-export default router
